919f2d16091a6a35f35451b374dfdbeab8ffdad5
Shadman Kolahzary <shadman.ko@gmail.com>
2016-01-06 21:17:34 +0330

    Update MergeSort.php

--- MergeSort.php
+++ MergeSort.php
@@ -1,33 +1,53 @@
-<?PHP
-$arrTest=array(5, 4, 3, 2, 1, 0);
-var_dump($arrTest);
-var_dump(mergeSort($arrTest));
+<?php
+$before=array();
+for($i=0;$i<rand(1,50);$i++)
+{
+    $before[]=rand(-100,100);
+}
+
+echo "<h3>Before:</h3>";
+echo json_encode($before);
+
+echo "<hr/><h3>Steps:</h3>";
+$after=mergeSort($before);
+
+echo "<hr/><h3>After:</h3>";
+echo json_encode($after);
 
 function mergeSort($array)
 {
-	if(count($array)<2) return $array;
-	
-	$mid=count($array)/2;
-	
-	$right=mergeSort(array_slice($array,0,$mid));
-	$left=mergeSort(array_slice($array,$mid));
-	
-	return merge($left,$right);
+    if(count($array)<2) return $array;
+
+    $mid=count($array)/2;
+    echo "<h5>Splitting ".json_encode($array)." ===> Left: ".json_encode(array_slice($array,0,$mid))." Right: ".json_encode(array_slice($array,$mid));
+
+    $right=mergeSort(array_slice($array,0,$mid));
+    $left=mergeSort(array_slice($array,$mid));
+
+    return merge($left,$right);
 }
 function merge($left,$right)
 {
-	$result=array();
-	while( count($left)>0 && count($right)>0 )
-	{
-		if($left[0] > $right[0])
-		{
-			array_push($result,array_shift($left));
-		}
-		else
-		{
-			array_push($result,array_shift($right));
-		}
-	}
-	return $result;
+    echo "<h5>Merging ".json_encode($left)." and ".json_encode($right)." ===> Result: ";
+    $result=array();
+    while( count($left)>0 || count($right)>0 )
+    {
+        if(count($left)>0 && count($right)>0)
+        {
+            if($left[0] < $right[0])
+            {
+                array_push($result,array_shift($left));
+            }
+            else
+            {
+                array_push($result,array_shift($right));
+            }
+        }else if(count($left)>0){
+            array_push($result,array_shift($left));
+        }else{
+            array_push($result,array_shift($right));
+        }
+    }
+    echo json_encode($result)."</h5>";
+    return $result;
 }
-?>
