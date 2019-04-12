ee2a1528683519439d539b6a762e553073f75aec
Shadman Kolahzary <shadman.ko@gmail.com>
2019-04-12 15:59:34 +0000

    Apply fixes from StyleCI

--- Greedy-Knapsak.php
+++ Greedy-Knapsak.php
@@ -23,18 +23,15 @@
 </head>
 <body>
 <form action="" method="post">
-    <?PHP
-        if(count($_REQUEST)==0)
-        {
+    <?php
+        if (count($_REQUEST) == 0) {
             echo '<p>How much items do we have in the store? <input type="number" name="count"/></p>';
         }
-        if(isset($_REQUEST['count']))
-        {
+        if (isset($_REQUEST['count'])) {
             echo '<p><input type="button" onclick="GenerateRandomValues()" value="Random fill"/></p>';
             echo '<p>Enter capacity of knapsack: <input type="number" name="capacity"/></p>';
             echo '<p>Allow franction? <input type="checkbox" name="fraction"/></p>';
-            for($i=1;$i<=$_REQUEST['count'];$i++)
-            {
+            for ($i = 1; $i <= $_REQUEST['count']; $i++) {
                 echo '<p>Item #'.$i.' ==> Weight: <input type="number" name="items['.$i.'][weight]"/> | Price: <input type="number" name="items['.$i.'][price]"/></p>';
             }
         }
@@ -43,56 +40,55 @@
 </form>
 <hr/>
 <?php
-if(!isset($_REQUEST['items'])) die();
+if (!isset($_REQUEST['items'])) {
+        die();
+    }
 
-$st=new store();
-$pack=new knapsack($_REQUEST['capacity']);
-$allowFraction=isset($_REQUEST['fraction']);
+$st = new store();
+$pack = new knapsack($_REQUEST['capacity']);
+$allowFraction = isset($_REQUEST['fraction']);
 
-foreach($_REQUEST['items'] as $index=>$item)
-{
-    $st->addItem(new item($item['weight'],$item['price']));
+foreach ($_REQUEST['items'] as $index=>$item) {
+    $st->addItem(new item($item['weight'], $item['price']));
 }
-print_knapsack($pack,'Before');
-print_store($st,'Before');
+print_knapsack($pack, 'Before');
+print_store($st, 'Before');
 echo '<br/>';
 
 $st->sortItemsByValue();
-print_store($st,'Sorted');
+print_store($st, 'Sorted');
 echo '<br/>';
-foreach($st->getAllItems() as $index=>$item) {
-    if ($pack->getFreeSpace() >= $item->getWeight())
-    {
+foreach ($st->getAllItems() as $index=>$item) {
+    if ($pack->getFreeSpace() >= $item->getWeight()) {
         $pack->addItem($st->pickItemByIndex($index));
-    }else{
-        if(!$allowFraction) continue;
-        $pack->addItem($st->pickItemFractionByIndex($index,$pack->getFreeSpace()));
+    } else {
+        if (!$allowFraction) {
+            continue;
+        }
+        $pack->addItem($st->pickItemFractionByIndex($index, $pack->getFreeSpace()));
     }
 
-
-    if($pack->getFreeSpace()==0)
-    {
+    if ($pack->getFreeSpace() == 0) {
         break;
     }
 }
-print_knapsack($pack,'Final');
-print_store($st,'Final');
+print_knapsack($pack, 'Final');
+print_store($st, 'Final');
 echo '<br/>';
 
-function print_store(store $store,$title='')
+function print_store(store $store, $title = '')
 {
     echo '<div><h3>'.$title.' Store</h3><hr/><b>--Info--</b><pre>';
     echo 'count of items:'.$store->getItemsCount().PHP_EOL;
     echo 'total price:'.$store->getTotalPrice().PHP_EOL;
     echo 'total weight:'.$store->getTotalWeight().PHP_EOL;
     echo '</pre><b>--Items--</b><table border="1"><tbody><tr><th>Price</th><th>Weight</th><th>Value</th></tr>';
-    foreach($store->getAllItems() as $index=>$item)
-    {
+    foreach ($store->getAllItems() as $index=>$item) {
         echo '<tr><td>'.$item->getPrice().'</td><td>'.$item->getWeight().'</td><td>'.$item->getValue().'</td></tr>';
     }
     echo '</tbody></table></div>';
 }
-function print_knapsack(knapsack $knapsack,$title='')
+function print_knapsack(knapsack $knapsack, $title = '')
 {
     echo '<div><h3>'.$title.' Knapsack</h3><hr/><b>--Info--</b><pre>';
     echo 'Capacity:'.$knapsack->getCapacity().PHP_EOL;
@@ -100,112 +96,189 @@ function print_knapsack(knapsack $knapsack,$title='')
     echo 'free space:'.$knapsack->getFreeSpace().PHP_EOL;
     echo 'total price:'.$knapsack->getTotalPrice().PHP_EOL;
     echo '</pre><b>--Items--</b><table border="1"><tbody><tr><th>Price</th><th>Weight</th><th>Value</th></tr>';
-    foreach($knapsack->getAllItems() as $index=>$item)
-    {
+    foreach ($knapsack->getAllItems() as $index=>$item) {
         echo '<tr><td>'.$item->getPrice().'</td><td>'.$item->getWeight().'</td><td>'.$item->getValue().'</td></tr>';
     }
     echo '</tbody></table></div>';
 }
-class store{
+class store
+{
     private $items;
-        public function sortItemsByValue(){ usort($this->items,itemCompare); }
-        public function getAllItems(){ return $this->items; }
-        public function getItemsCount(){ return count($this->items); }
-        public function getItemByIndex($index){ return $this->items[$index]; }
-        public function addItem(item $item){ $this->items[] = $item;}
+
+    public function sortItemsByValue()
+    {
+        usort($this->items, itemCompare);
+    }
+
+    public function getAllItems()
+    {
+        return $this->items;
+    }
+
+    public function getItemsCount()
+    {
+        return count($this->items);
+    }
+
+    public function getItemByIndex($index)
+    {
+        return $this->items[$index];
+    }
+
+    public function addItem(item $item)
+    {
+        $this->items[] = $item;
+    }
+
     public function __construct()
     {
-        $this->items=array();
+        $this->items = [];
     }
-    public function pickItemFractionByIndex($index,$HowMutch)
+
+    public function pickItemFractionByIndex($index, $HowMutch)
     {
-        $price=$this->items[$index]->getValue()*$HowMutch;
-        $item=new item($HowMutch,$price);
-        $this->items[$index]->setWeight($this->items[$index]->getWeight()-$HowMutch);
-        $this->items[$index]->setPrice($this->items[$index]->getPrice()-$price);
+        $price = $this->items[$index]->getValue() * $HowMutch;
+        $item = new item($HowMutch, $price);
+        $this->items[$index]->setWeight($this->items[$index]->getWeight() - $HowMutch);
+        $this->items[$index]->setPrice($this->items[$index]->getPrice() - $price);
+
         return $item;
     }
-    public function pickItemByIndex($index){
-        $item=$this->items[$index];
+
+    public function pickItemByIndex($index)
+    {
+        $item = $this->items[$index];
         unset($this->items[$index]);
+
         return $item;
     }
-    public function getTotalWeight(){
-        $sum=0;
-        foreach($this->items as $index=>$item)
-        {
-            $sum+=$item->getWeight();
+
+    public function getTotalWeight()
+    {
+        $sum = 0;
+        foreach ($this->items as $index=>$item) {
+            $sum += $item->getWeight();
         }
+
         return $sum;
     }
-    public function getTotalPrice(){
-        $sum=0;
-        foreach($this->items as $index=>$item)
-        {
-            $sum+=$item->getPrice();
+
+    public function getTotalPrice()
+    {
+        $sum = 0;
+        foreach ($this->items as $index=>$item) {
+            $sum += $item->getPrice();
         }
+
         return $sum;
     }
 }
-class knapsack{
+class knapsack
+{
     private $capacity;
-        public function getCapacity(){ return $this->capacity; }
-        public function setCapacity($capacity){ $this->capacity = $capacity; }
+
+    public function getCapacity()
+    {
+        return $this->capacity;
+    }
+
+    public function setCapacity($capacity)
+    {
+        $this->capacity = $capacity;
+    }
+
     private $items;
-        public function getAllItems(){ return $this->items; }
-        public function addItem(item $item)
-        {
-            if ($item->getWeight() + $this->getUsedWeight() > $this->capacity) {
-                echo "Error: Knapsack overloaded..." . PHP_EOL;
-                return;
-            }
-            $this->items[] = $item;
+
+    public function getAllItems()
+    {
+        return $this->items;
+    }
+
+    public function addItem(item $item)
+    {
+        if ($item->getWeight() + $this->getUsedWeight() > $this->capacity) {
+            echo 'Error: Knapsack overloaded...'.PHP_EOL;
+
+            return;
         }
+        $this->items[] = $item;
+    }
+
     public function __construct($capacity)
     {
         $this->capacity = $capacity;
-        $this->items = array();
+        $this->items = [];
     }
-    public function getTotalPrice(){
-        $sum=0;
-        foreach($this->items as $index=>$item)
-        {
-            $sum+=$item->getPrice();
+
+    public function getTotalPrice()
+    {
+        $sum = 0;
+        foreach ($this->items as $index=>$item) {
+            $sum += $item->getPrice();
         }
+
         return $sum;
     }
-    public function getUsedWeight(){
-        $sum=0;
-        foreach($this->items as $index=>$item)
-        {
-            $sum+=$item->getWeight();
+
+    public function getUsedWeight()
+    {
+        $sum = 0;
+        foreach ($this->items as $index=>$item) {
+            $sum += $item->getWeight();
         }
+
         return $sum;
     }
-    public function getFreeSpace(){
-        return $this->getCapacity()-$this->getUsedWeight();
+
+    public function getFreeSpace()
+    {
+        return $this->getCapacity() - $this->getUsedWeight();
     }
 }
-class item{
+class item
+{
     private $weight;
-        public function getWeight(){ return $this->weight; }
-        public function setWeight($weight){ $this->weight = $weight; }
-    private $price;
-        public function getPrice(){ return $this->price; }
-        public function setPrice($price){ $this->price = $price; }
-    public function __construct($weight,$price)
+
+    public function getWeight()
     {
-        $this->weight=$weight;
-        $this->price=$price;
+        return $this->weight;
     }
-    public function getValue(){
+
+    public function setWeight($weight)
+    {
+        $this->weight = $weight;
+    }
+
+    private $price;
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function __construct($weight, $price)
+    {
+        $this->weight = $weight;
+        $this->price = $price;
+    }
+
+    public function getValue()
+    {
         return $this->price / $this->weight;
     }
 }
-function itemCompare($first,$second)
+function itemCompare($first, $second)
 {
-    if($first->getValue() == $second->getValue()) return 0;
-    return ($first->getValue() < $second->getValue())?1:-1;
+    if ($first->getValue() == $second->getValue()) {
+        return 0;
+    }
+
+    return ($first->getValue() < $second->getValue()) ? 1 : -1;
 }
 ?>
 </body>
--- MergeSort-3parts.php
+++ MergeSort-3parts.php
@@ -1,43 +1,56 @@
 <?php
-$before=array();
-for($i=0;$i<rand(1,50);$i++)
-{
-    $before[]=rand(-100,100);
+
+$before = [];
+for ($i = 0; $i < rand(1, 50); $i++) {
+    $before[] = rand(-100, 100);
 }
 
-echo "<h3>Before:</h3>";
+echo '<h3>Before:</h3>';
 echo json_encode($before);
 
-echo "<hr/><h3>Steps:</h3>";
-$after=mergeSort($before);
+echo '<hr/><h3>Steps:</h3>';
+$after = mergeSort($before);
 
-echo "<hr/><h3>After:</h3>";
+echo '<hr/><h3>After:</h3>';
 echo json_encode($after);
 
 function mergeSort($array)
 {
-    if(count($array)<2) return $array;
-    if(count($array)<3){
-        echo "<h5>Splitting ".json_encode($array)." ===>"." Part #1: ".json_encode(array($array[0]))." Part #2: ".json_encode(array($array[1]));
-        return merge(array($array[0]),array($array[1]));
+    if (count($array) < 2) {
+        return $array;
     }
-    $part=(int)(count($array)/3);
-    echo "<h5>Splitting ".json_encode($array)." ===>"." Part #1: ".json_encode(array_slice($array,0,$part))." Part #2: ".json_encode(array_slice($array,$part,$part))." Part #3: ".json_encode(array_slice($array,$part*2));
-    return merge(mergeSort(array_slice($array,0,$part)),mergeSort(array_slice($array,$part,$part)),mergeSort(array_slice($array,$part*2)));
+    if (count($array) < 3) {
+        echo '<h5>Splitting '.json_encode($array).' ===>'.' Part #1: '.json_encode([$array[0]]).' Part #2: '.json_encode([$array[1]]);
+
+        return merge([$array[0]], [$array[1]]);
+    }
+    $part = (int) (count($array) / 3);
+    echo '<h5>Splitting '.json_encode($array).' ===>'.' Part #1: '.json_encode(array_slice($array, 0, $part)).' Part #2: '.json_encode(array_slice($array, $part, $part)).' Part #3: '.json_encode(array_slice($array, $part * 2));
+
+    return merge(mergeSort(array_slice($array, 0, $part)), mergeSort(array_slice($array, $part, $part)), mergeSort(array_slice($array, $part * 2)));
 }
-function merge($part1,$part2,$part3=null)
+function merge($part1, $part2, $part3 = null)
 {
-    if($part3==null)
-    {
-        echo "<h5>Merging ".json_encode($part1)." and ".json_encode($part2)." ===> Result: ";
-        $result=array();
-        while( count($part1)>0 || count($part2)>0 )
-            if(count($part1)>0 && count($part2)>0)
-                if($part1[0] < $part2[0]) array_push($result,array_shift($part1));
-                else array_push($result,array_shift($part2));
-            else if(count($part1)>0) array_push($result,array_shift($part1));
-            else array_push($result,array_shift($part2));
-        echo json_encode($result)."</h5>";
+    if ($part3 == null) {
+        echo '<h5>Merging '.json_encode($part1).' and '.json_encode($part2).' ===> Result: ';
+        $result = [];
+        while (count($part1) > 0 || count($part2) > 0) {
+            if (count($part1) > 0 && count($part2) > 0) {
+                if ($part1[0] < $part2[0]) {
+                    array_push($result, array_shift($part1));
+                } else {
+                    array_push($result, array_shift($part2));
+                }
+            } elseif (count($part1) > 0) {
+                array_push($result, array_shift($part1));
+            } else {
+                array_push($result, array_shift($part2));
+            }
+        }
+        echo json_encode($result).'</h5>';
+
         return $result;
-    }else return merge( merge($part1,$part2),$part3);
+    } else {
+        return merge(merge($part1, $part2), $part3);
+    }
 }
